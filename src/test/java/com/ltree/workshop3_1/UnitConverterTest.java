e045d825bafb7aa227305581af774fd3923c0efb
Peter Karas <peter@kitkline.co.uk>
2013-12-06 13:46:37 +0000

    update POM and add first test
    Specify Java 1.6 and update version of JUnit

---
+++ src/test/java/com/ltree/workshop3_1/UnitConverterTest.java
@@ -0,0 +1,38 @@
+package com.ltree.workshop3_1;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UnitConverterTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testMilesToKilometers() {
+        //fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPoundsToKilograms() {
+        //fail("Not yet implemented");
+    }
+
+    @Test
+    public void testKilometersToMiles() {
+        //fail("Not yet implemented");
+    }
+
+    @Test
+    public void testKilogramsToPounds() {
+       // fail("Not yet implemented");
+    }
+
+}
