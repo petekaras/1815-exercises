b523ea625ae53acfcfe941757a42e412b986f652
Peter Karas <peter@kitkline.co.uk>
2013-12-06 15:34:20 +0000

    Passing Unit tests

--- src/main/java/com/ltree/workshop3_1/UnitConverter.java
+++ src/main/java/com/ltree/workshop3_1/UnitConverter.java
@@ -5,17 +5,19 @@ package com.ltree.workshop3_1;
  *
  */
 public class UnitConverter {
-
+    public static final double ONE_MILE_AS_KILOMETER = 1.609344;
+    public static final double ONE_KILOGRAM_AS_POUNDS = 2.20462;
+    
     public static double  milesToKilometers(double miles) {
-        return 0;
+        return miles * ONE_MILE_AS_KILOMETER;
     }
     public static double  poundsToKilograms(double pounds) {
-        return 0;
+        return (1/ONE_KILOGRAM_AS_POUNDS)*pounds;
     }   
     public static double  kilometersToMiles(double kilometers) {
-        return 0;
+        return (1/ONE_MILE_AS_KILOMETER)*kilometers;
     }   
     public static double  kilogramsToPounds(double kilograms) {
-        return 0;
+        return kilograms * ONE_KILOGRAM_AS_POUNDS;
     }      
 }
--- src/test/java/com/ltree/workshop3_1/UnitConverterTest.java
+++ src/test/java/com/ltree/workshop3_1/UnitConverterTest.java
@@ -1,6 +1,5 @@
 package com.ltree.workshop3_1;
 
-
 import org.junit.Assert;
 
 import org.junit.After;
@@ -11,9 +10,18 @@ public class UnitConverterTest {
     private static final double MILES = 23.78;
     private static final double KILOMETERS = 38.27;
     private static final double KILOGRAMS = 455.30;
-    private static final double POUNDS = 1003.78;
-    
-    private static final double ACCURACY = 0.01;
+    private static final double POUNDS = 1003.76;
+    /**
+     * Epsilon is the value that the 2 numbers can be off by. This accounts for
+     * rounding errors in the calculation. Note each unit has its different
+     * degree of variance, so this is a nice way to express how accurate the
+     * results are.
+     */
+    private static final double EPSILON_MILES = 0.01;
+    private static final double EPSILON_KILOMETERS = 0.01;
+    private static final double EPSILON_KILOGRAMS = 0.01;
+    private static final double EPSILON_POUNDS = 0.01;
+
     @Before
     public void setUp() throws Exception {
     }
@@ -23,27 +31,39 @@ public class UnitConverterTest {
     }
 
     @Test
+    public void shouldConvertBasicKilogramUnit() {
+        double result = UnitConverter.kilogramsToPounds(1);
+        Assert.assertEquals(UnitConverter.ONE_KILOGRAM_AS_POUNDS, result, EPSILON_POUNDS);
+    }
+
+    @Test
+    public void shouldConvertBasicMileUnit() {
+        double result = UnitConverter.milesToKilometers(1);
+        Assert.assertEquals(UnitConverter.ONE_MILE_AS_KILOMETER, result, EPSILON_KILOMETERS);
+    }
+
+    @Test
     public void shouldConvertMilesToKilometers() {
-       double result = UnitConverter.milesToKilometers(MILES);
-       Assert.assertEquals(KILOMETERS, result, ACCURACY);       
+        double result = UnitConverter.milesToKilometers(MILES);
+        Assert.assertEquals(KILOMETERS, result, EPSILON_KILOMETERS);
     }
 
     @Test
     public void shouldConvertPoundsToKilograms() {
         double result = UnitConverter.poundsToKilograms(POUNDS);
-        Assert.assertEquals(KILOGRAMS, result,ACCURACY);
+        Assert.assertEquals(KILOGRAMS, result, EPSILON_KILOGRAMS);
     }
 
     @Test
     public void shouldConvertKilometersToMiles() {
         double result = UnitConverter.kilometersToMiles(KILOMETERS);
-        Assert.assertEquals(MILES, result, ACCURACY);     
+        Assert.assertEquals(MILES, result, EPSILON_MILES);
     }
 
     @Test
     public void shouldConvertKilogramsToPounds() {
-        double result = UnitConverter.kilogramsToPounds(POUNDS);
-        Assert.assertEquals(POUNDS, result,KILOGRAMS);
+        double result = UnitConverter.kilogramsToPounds(KILOGRAMS);
+        Assert.assertEquals(POUNDS, result, EPSILON_POUNDS);
     }
 
 }
