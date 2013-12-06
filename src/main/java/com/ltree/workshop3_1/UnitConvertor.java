0766bb428a4658b3054ce5038d63272395f14a1f
Peter Karas <peter@kitkline.co.uk>
2013-12-06 13:14:52 +0000

    First implementation

---
+++ src/main/java/com/ltree/workshop3_1/UnitConvertor.java
@@ -0,0 +1,21 @@
+package com.ltree.workshop3_1;
+/**
+ * Simple Convertor Utility class.
+ * @author peter
+ *
+ */
+public class UnitConvertor {
+
+    public static double  milesToKilometers() {
+        return 0;
+    }
+    public static double  poundsToKilograms() {
+        return 0;
+    }   
+    public static double  kilometersToMiles() {
+        return 0;
+    }   
+    public static double  kilogramsToPounds() {
+        return 0;
+    }      
+}
